96a99d86a822414365e78ffebf81a5dd5807f69f
Ascellapp <info@ascellapp.com>
2019-05-17 14:35:53 +0300

    Weight_diary
    +1

---
+++ app/src/main/java/com/ascellapp/weightdiary/Authentication.kt
@@ -0,0 +1,12 @@
+package com.ascellapp.weightdiary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Authentication : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+    }
+}
