8b9466d2a8b4716e993436ddac24e76b496384c4
Ascellapp <info@ascellapp.com>
2019-05-19 18:43:46 +0300

    формула BMI кг+см

---
+++ app/src/main/java/com/ascellapp/weightdiary/BMIformula.kt
@@ -0,0 +1,21 @@
+package com.ascellapp.weightdiary
+
+fun bodyMassIndex() {
+
+    val height = 1.7 // meters
+    val weight = 80 // kilograms
+
+    println("Height: $height meters")
+    println("Weight: $weight kg")
+    println()
+
+    val bmi = weight / (height * height)
+    println("BMI = %.3f".format(bmi))
+
+    if(bmi < 25)
+        print("Normal.")
+    else if (bmi <= 29)
+        print("Overweight.")
+    else
+        print("Obese!")
+}
